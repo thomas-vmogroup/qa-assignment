8dba088c07193b257a8f9e35f1dc8f784f5bd32f
Thomas Le <thomas@vmogroup.com>
2023-08-09 17:13:42 +0700

    Organize the test step

--- steps/ImageSteps.ts
+++ steps/ImageSteps.ts
@@ -12,6 +12,11 @@ export default class ImageSteps extends BaseAPI {
         return this.response;
     }
 
+    public async uploadAnImageWithWrongEndpoint(file) {
+        this.response = await this.doPost(EndPoint.WRONG_ENDPOINT, file, MimeType.IMAGE);
+        return this.response;
+    }
+
     //Verify the permanent image link is right format. E.g: https://assessement.onrender.com/images/3b22d180-b343-4d66-b24a-ef843241cdcc.png
     public verifyTheImageIsRightFormat(url: string) {
         expect(url).toMatch(this.imageLinkRegex);
--- tests/us2_api-zip.spec.ts
+++ tests/us2_api-zip.spec.ts
@@ -2,29 +2,28 @@ import path from "path";
 import { Path } from "../const/Path";
 import { Utilities } from "../util/Utilities";
 import { test } from "./fixtures/BaseStep";
+import { FileName } from "../const/FileName";
 
 let Util = new Utilities();
+let response;
 /*
     User Story 2 - In order to save my time from uploading my pictures multiple times via 
     https://assessement.onrender.com/api/zip API service: As an Anonymous user, I want to attach a zip file 
     containing multiple images and I want each of these uploaded images to have a permanent link.
 */
 
-test.describe('Upload zip file', () => {
-
-    test.beforeEach(async ({ }) => {
-    });
-
+test.describe('User Story 2| Upload zip function', () => {
 
     /*
         TC_01: Should allow guest to upload an zip file contain multiple image files
         Precondition: 
             - A zip file including the image file is provided
         Test Step: 
-            - Send the post request to upload the zip file
+            - Send the "POST | /api/zip" request to upload the zip file
             - Verify the status code should be 200
+            - Verify the response body has property "images"
             - Read the input zip file and get the list of file name.
-            - Verify each of file from response body image list should be accessible: Send GET request and validate response code = 200
+            - Verify each of file from response body image list should be accessible: Send GET |/images/<return-image>.<extention> request and validate response code = 200
             - Verify the permanen file format for each file is coresponding with the zip temp folder:
                 + The number of image files inside the zip file = the number of file from response body
                 + The format of file should be coresponsding
@@ -32,23 +31,35 @@ test.describe('Upload zip file', () => {
             - temp folder should be deleted
     */
     test('Should allow guest to upload an zip file contain multiple image files', async ({ zipSteps, commonSteps, imageSteps }) => {
-        const zipFilePath = path.resolve(Path.ZIPS, "images-only.zip");
-        const lisFileNameFromZipFile = await Util.readZipArchive(zipFilePath)
+        let responseImageList;
+        const zipFilePath = path.resolve(Path.ZIPS, FileName.ZIP_FILE);
+        const lisFileNameFromZipFile = Util.readZipArchive(zipFilePath)
 
-        const response = await zipSteps.uploadAZipFile(zipFilePath);
-        await commonSteps.verifyStatusCodeIs(200, response);
-        await commonSteps.verifyHaveProperty("images", response);
-        await zipSteps.verifyLisOfImageAreCorrectFormat();
+        await test.step(`Send the "POST | /api/zip" request to upload the zip file`, async () => {
+            response = await zipSteps.uploadAZipFile(zipFilePath);
+        })
 
-        //Verify each of file from response body image list should be accessible: Send GET request and validate response code = 200
-        const responseImageList = await zipSteps.getTheResponseImageList();
-        for (var i = 0; i < responseImageList.length; i++) {
-            await imageSteps.verifyTheImageIsAvailabe(responseImageList[i]);
-        }
+        await test.step(`Verify the status code should be 200`, async () => {
+            await commonSteps.verifyStatusCodeIs(200, response);
+        })
 
-        await zipSteps.verifyZipfileAndResponeImagesAreCorresponding(lisFileNameFromZipFile)
+        await test.step(`Verify the response body has property "images"`, async () => {
+            await commonSteps.verifyHaveProperty("images", response);
+        })
 
+        await test.step(`Verify list of images are correct format: https://assessement.onrender.com/images/<UUID>.<extention>`, async () => {
+            await zipSteps.verifyLisOfImageAreCorrectFormat();
+        })
 
+        await test.step(`Verify each of file from response body image list should be accessible`, async () => {
+            responseImageList = await zipSteps.getTheResponseImageList();
+            for (var i = 0; i < responseImageList.length; i++) {
+                await imageSteps.verifyTheImageIsAvailabe(responseImageList[i]);
+            }
+        })
+        await test.step(`Verify the permanen file format for each file is coresponding with the zip temp folder`, async () => {
+            await zipSteps.verifyZipfileAndResponeImagesAreCorresponding(lisFileNameFromZipFile)
+        })
     });
 
     /*
@@ -56,15 +67,49 @@ test.describe('Upload zip file', () => {
     Precondition:
         - A non zip file is provided (e.g: file.csv)
     Test Step: 
-        - Provide the file is not a zip file
-        - Send the post request to upload the zip file
+        - Send the "POST | /api/zip" request to upload the file
         - Verify the status code should be 403
-        - Verify the body response should have "err" field with the message: File isn' a zip
+        - Verify the body response should have "err" field with the message: "File isn' a zip"
     */
     test('Should not allow guest to upload the file is not a zip', async ({ zipSteps, commonSteps }) => {
-        const file = path.resolve(Path.OTHERS, "username.csv");
-        const response = await zipSteps.uploadAZipFile(file);
-        await commonSteps.verifyStatusCodeIs(403, response);
-        await commonSteps.verifyErrorMessage("File isn' a zip", response);
+        const file = path.resolve(Path.OTHERS, FileName.CSV_FILE);
+        await test.step(`Send the "POST | /api/zip" request to upload the file`, async () => {
+            response = await zipSteps.uploadAZipFile(file);
+        })
+
+        await test.step(`Verify the status code should be 403`, async () => {
+            await commonSteps.verifyStatusCodeIs(403, response);
+        })
+
+        await test.step(`Verify the body response should have "err" field with the message: "File isn' a zip"`, async () => {
+            await commonSteps.verifyErrorMessage("File isn' a zip", response);
+        })
+
+    });
+
+    /*
+    Tescase 02: Should allow guest to upload the file is zip file which is not contain any image file
+    Precondition:
+        - A valid zip file is provided
+    Test Step: 
+        - Send the POST | /api/zip request to upload the zip file
+        - Verify the status code should be 500
+        - Verify the body response should have "err" field with the message: "no image found in zip file"
+    */
+    test('Should allow guest to upload the file is zip file which is not contain any image file', async ({ zipSteps, commonSteps }) => {
+        const file = path.resolve(Path.ZIPS, FileName.NOT_CONTAIN_IMAGE_FILE);
+
+        await test.step(`Send the "POST | /api/zip" request to upload the zip file`, async () => {
+            response = await zipSteps.uploadAZipFile(file);
+        })
+
+
+        await test.step(`Verify the status code should be 500`, async () => {
+            await commonSteps.verifyStatusCodeIs(500, response);
+        })
+
+        await test.step(`Verify the body response should have "err" field with the message: "no image found in zip file"`, async () => {
+            await commonSteps.verifyErrorMessage("no image found in zip file", response);
+        })
     });
 });
