9ea5a99672873c955e45e69f96227c7b5b75d73c
Thomas Le <thomas@vmogroup.com>
2023-08-09 17:32:05 +0700

    Update test case file

--- tests/us2_api-zip.spec.ts
+++ tests/us2_api-zip.spec.ts
@@ -23,12 +23,11 @@ test.describe('User Story 2| Upload zip function', () => {
             - Verify the status code should be 200
             - Verify the response body has property "images"
             - Read the input zip file and get the list of file name.
+            - Verify list of images are correct format: https://assessement.onrender.com/images/<UUID>.<extention>
             - Verify each of file from response body image list should be accessible: Send GET |/images/<return-image>.<extention> request and validate response code = 200
-            - Verify the permanen file format for each file is coresponding with the zip temp folder:
+            - Verify the permanen file format for each file is coresponding with the zip file:
                 + The number of image files inside the zip file = the number of file from response body
                 + The format of file should be coresponsding
-        Postcondition:
-            - temp folder should be deleted
     */
     test('Should allow guest to upload an zip file contain multiple image files', async ({ zipSteps, commonSteps, imageSteps }) => {
         let responseImageList;
@@ -67,13 +66,13 @@ test.describe('User Story 2| Upload zip function', () => {
     Precondition:
         - A non zip file is provided (e.g: file.csv)
     Test Step: 
-        - Send the "POST | /api/zip" request to upload the file
+        - Send the "POST | /api/zip" request with any non zip file
         - Verify the status code should be 403
         - Verify the body response should have "err" field with the message: "File isn' a zip"
     */
     test('Should not allow guest to upload the file is not a zip', async ({ zipSteps, commonSteps }) => {
         const file = path.resolve(Path.OTHERS, FileName.CSV_FILE);
-        await test.step(`Send the "POST | /api/zip" request to upload the file`, async () => {
+        await test.step(`Send the "POST | /api/zip" request with any non zip file`, async () => {
             response = await zipSteps.uploadAZipFile(file);
         })
 
@@ -88,9 +87,9 @@ test.describe('User Story 2| Upload zip function', () => {
     });
 
     /*
-    Tescase 02: Should allow guest to upload the file is zip file which is not contain any image file
+    Tescase 03: Should allow guest to upload the file is zip file which is not contain any image file
     Precondition:
-        - A valid zip file is provided
+        - A zip file which is not contain any image file is provided
     Test Step: 
         - Send the POST | /api/zip request to upload the zip file
         - Verify the status code should be 500
